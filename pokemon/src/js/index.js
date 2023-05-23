5ad1182b38845a15d550684f73ac9779330abf33
Kauê Lucena da Silva <126902347+LucenadoisK@users.noreply.github.com>
2023-05-22 21:49:45 -0300

    up

--- pokemon/src/js/index.js
+++ pokemon/src/js/index.js
@@ -7,18 +7,12 @@ const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
-    if(modoEscuroEstaAtivo) {
-        body.classList.remove("modo-escuro");
+    body.classList.toggle("modo-escuro")
 
+    if(modoEscuroEstaAtivo) {
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
     } else {
-        body.classList.add("modo-escuro");
-
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
     
-
     }
-
-   
-
 });
